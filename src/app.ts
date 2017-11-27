eea97c4f3aa812cb8f787ed8df92e69152ccce71
Eamonn de Leastar <edeleastar@wit.ie>
2017-11-27 17:35:46 +0000

    index + initial app viewmodel

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
+import { inject, Aurelia } from 'aurelia-framework';
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+@inject(Aurelia, EventAggregator)
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      {
+        route: ['', 'login'],
+        name: 'login',
+        moduleId: 'components/login',
+        nav: true,
+        title: 'Login',
+      },
+      {
+        route: 'signup',
+        name: 'signup',
+        moduleId: 'components/signup',
+        nav: true,
+        title: 'Signup',
+      },
+    ]);
+    this.router = router;
+  }
 }
