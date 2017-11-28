9476d439eb314452fd777584652bb96327f7ff67
Eamonn de Leastar <edeleastar@wit.ie>
2017-11-28 18:46:33 +0000

    simplify fixtures

--- src/services/fixtures.ts
+++ src/services/fixtures.ts
@@ -3,44 +3,4 @@ import { Candidate, Donation, User } from './models';
 export default class Fixtures {
   baseUrl = 'http://localhost:4000';
   methods = ['Cash', 'PayPal'];
-
-  candidates: Array<Candidate> = [
-    {
-      firstName: 'Lisa',
-      lastName: 'Simpson',
-      office: 'President',
-    },
-    {
-      firstName: 'Bart',
-      lastName: 'Simpson',
-      office: 'President',
-    },
-  ];
-
-  donations: Array<Donation> = [
-    {
-      amount: 23,
-      method: 'cash',
-      candidate: this.candidates[0],
-    },
-    {
-      amount: 212,
-      method: 'paypal',
-      candidate: this.candidates[1],
-    },
-  ];
-
-  users: Map<string, User> = new Map()
-    .set('homer@simpson.com', {
-      firstName: 'Homer',
-      lastName: 'Simpson',
-      email: 'homer@simpson.com',
-      password: 'secret',
-    })
-    .set('marge@simpson.com', {
-      firstName: 'Marge',
-      lastName: 'Simpson',
-      email: 'marge@simpson.com',
-      password: 'secret',
-    });
 }
