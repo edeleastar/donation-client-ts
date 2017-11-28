07c42c07173d36b3de68a94eff1575009453d157
Eamonn de Leastar <edeleastar@wit.ie>
2017-11-28 18:47:18 +0000

    retrieve candidates + user from donation-web api. post donations to api

--- src/services/donation-service.ts
+++ src/services/donation-service.ts
@@ -1,42 +1,64 @@
 import { inject } from 'aurelia-framework';
+import AsyncHttpClient from './async-http-client';
 import Fixtures from './fixtures';
 import { TotalUpdate, LoginStatus } from './messages';
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { Candidate, Donation, User } from './models';
 
-@inject(Fixtures, EventAggregator)
+@inject(Fixtures, EventAggregator, AsyncHttpClient)
 export class DonationService {
   ea: EventAggregator;
+  ac: AsyncHttpClient;
   donations: Array<Donation> = [];
   methods: Array<string> = [];
   candidates: Array<Candidate> = [];
-  users: Map<string, User>;
+  users: Map<string, User> = new Map();
   total = 0;
 
-  constructor(data: Fixtures, ea: EventAggregator) {
-    this.users = data.users;
-    this.donations = data.donations;
-    this.candidates = data.candidates;
+  constructor(data: Fixtures, ea: EventAggregator, ac: AsyncHttpClient) {
     this.methods = data.methods;
     this.ea = ea;
+    this.ac = ac;
+    this.getCandidates();
+    this.getUsers();
+  }
+
+  getCandidates() {
+    this.ac.get('/api/candidates').then(res => {
+      this.candidates = res.content;
+    });
+  }
+
+  getUsers() {
+    this.ac.get('/api/users').then(res => {
+      const users = res.content as Array<User>;
+      users.forEach(user => {
+        this.users.set(user.email, user);
+      });
+    });
   }
 
   donate(amount: number, method: string, candidate: Candidate) {
     const donation = {
       amount: amount,
       method: method,
-      candidate: candidate,
     };
-    this.donations.push(donation);
-    console.log(
-      `${amount} donated to ${candidate.firstName} ${candidate.lastName} : ${
-        method
-      }`,
-    );
+    this.ac
+      .post('/api/candidates/' + candidate._id + '/donations', donation)
+      .then(res => {
+        let returnedDonation = res.content as Donation;
+        returnedDonation.candidate = candidate;
+        this.donations.push(returnedDonation);
+        console.log(
+          `${amount} donated to ${candidate.firstName} ${
+            candidate.lastName
+            } : ${method}`,
+        );
 
-    this.total = this.total + amount;
-    console.log('Total so far ' + this.total);
-    this.ea.publish(new TotalUpdate(this.total));
+        this.total = this.total + amount;
+        console.log('Total so far ' + this.total);
+        this.ea.publish(new TotalUpdate(this.total));
+      });
   }
 
   addCandidate(firstName: string, lastName: string, office: string) {
