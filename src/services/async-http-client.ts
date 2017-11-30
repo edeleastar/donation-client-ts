7ed8188d60c4f8e0e7c4e31b3f146e1da9939203
Eamonn de Leastar <edeleastar@wit.ie>
2017-11-30 17:25:25 +0000

    equip async-http-client with jwt support

--- src/services/async-http-client.ts
+++ src/services/async-http-client.ts
@@ -1,27 +1,72 @@
 import { inject } from 'aurelia-framework';
 import { HttpClient } from 'aurelia-http-client';
 import Fixtures from './fixtures';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { LoginStatus } from './messages';
+import { User } from './models';
 
 @inject(HttpClient, Fixtures)
 export default class AsyncHttpClient {
   http: HttpClient;
+  ea: EventAggregator;
 
-  constructor(httpClient, fixtures) {
+  constructor(httpClient, fixtures, ea) {
     this.http = httpClient;
     this.http.configure(http => {
       http.withBaseUrl(fixtures.baseUrl);
     });
+    this.ea = ea;
   }
 
-  get(url) {
+  isAuthenticated() {
+    let authenticated = false;
+    if (localStorage.donation !== 'null') {
+      authenticated = true;
+      this.http.configure(http => {
+        const auth = JSON.parse(localStorage.donation);
+        http.withHeader('Authorization', 'bearer ' + auth.token);
+      });
+    }
+    return authenticated;
+  }
+
+  authenticate(url: string, credentials: {email:string, password:string}) {
+    this.http
+      .post(url, credentials)
+      .then(response => {
+        const status = response.content;
+        if (status.success) {
+          localStorage.donation = JSON.stringify(response.content);
+          this.http.configure(configuration => {
+            configuration.withHeader(
+              'Authorization',
+              'bearer ' + response.content.token,
+            );
+          });
+        }
+        this.ea.publish(new LoginStatus(true));
+      })
+      .catch(error => {
+        this.ea.publish(new LoginStatus(false, 'service not available'));
+      });
+  }
+
+  clearAuthentication() {
+    localStorage.donation = null;
+    this.http.configure(configuration => {
+      configuration.withHeader('Authorization', '');
+    });
+  }
+
+  get(url: string) {
     return this.http.get(url);
   }
 
-  post(url, obj) {
+  post(url: string, obj: any) {
     return this.http.post(url, obj);
   }
 
-  delete(url) {
+  delete(url: string) {
     return this.http.delete(url);
   }
 }
